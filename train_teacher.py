cc93c03a71c5dbab7a1340e99dc3412dc372d151
Your Name <dboctor18@gmail.com>
2021-11-15 15:02:39 -0500

    second commit

--- train_teacher.py
+++ train_teacher.py
@@ -30,12 +30,9 @@ def train_teacher(net, trainloader, lr, wd, epochs, momentum, milestones, gamma,
             loss.backward()
             optimizer.step()
             
-            # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                    (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+            
+        print(f"EPOCH {epoch+1}: {running_loss/len(trainloader)}")
 
     print('Finished Training')
 
